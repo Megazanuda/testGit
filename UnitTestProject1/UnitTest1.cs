eadb72f3eae3f91e0ac67323dd4525079ec28cf5
Menkov L <menkovwork@gmail.com>
2024-12-24 22:51:05 +0300

    as

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ConsoleApp2;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
