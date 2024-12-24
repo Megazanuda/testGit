80faa820254401199e946fa05ae496d977f95d83
Menkov L <menkovwork@gmail.com>
2024-12-24 22:28:04 +0300

    ddf

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,7 +6,8 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello !");
+            Console.WriteLine("Hello adad !");
+            Console.WriteLine("Helloasdadad !");
         }
     }
 }
